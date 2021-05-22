cae36a96120b4b543604cb8e3d79f4f11bdea4aa
FeiChan <chfc@student.unimelb.edu.au>
2021-05-22 15:43:18 +1000

    fixed master pin delete unuse comment

--- main.adb
+++ main.adb
@@ -42,25 +42,26 @@ begin
    Stack.Init_Stack(calStack);
    
    -- Checking if a master password is provided
-   if MyCommandLine.Argument_Count = 1 and then MyCommandLine.Argument(1)'Length = 4 then
-      declare
-         P: String(1..4) := MyCommandLine.Argument(1);
-      begin
-         -- check if the argument string is PIN
-         if passwordmanager.IsPin(P) then
-            PIN2 := PIN.From_String(P);
-            -- check if the PINs are equal
-            if PIN."=" (PIN1, PIN2) then
-               isLocked := True;
-            else 
-            Put_Line ("Invalid Master Password!");
-            return;
+   if MyCommandLine.Argument_Count = 1 then
+      if MyCommandLine.Argument(1)'Length = 4 then
+         declare
+            P: String(1..4) := MyCommandLine.Argument(1);
+         begin
+            -- check if the argument string is PIN
+            if passwordmanager.IsPin(P) then
+               PIN2 := PIN.From_String(P);
+               -- check if the PINs are equal
+               if not PIN."=" (PIN1, PIN2) then
+                  Put_Line ("Invalid Master Password!");
+                  return;
+               end if;
+            else
+               Put_Line ("Invalid Master Password!");
+               return;
             end if;
-         else
-         Put_Line ("Invalid Master Password!");
-         return;
-         end if;
-      end;
+         end;
+      end if;
+      
    else
       Put_Line ("Master Password Not Provided!");
       return;
@@ -200,81 +201,5 @@ begin
          end if;
       end;
    end loop;
-
-   
-   
-   -- Put(MyCommandLine.Command_Name); Put_Line(" is running!");
-   -- Put("I was invoked with "); Put(MyCommandLine.Argument_Count,0); Put_Line(" arguments.");
-   -- for Arg in 1..MyCommandLine.Argument_Count loop
-   --    Put("Argument "); Put(Arg,0); Put(": """);
-   --    Put(MyCommandLine.Argument(Arg)); Put_Line("""");
-   -- end loop;
-
-   -- VariableStore.Init(DB);
-   -- Put_Line("Adding an entry to the database");
-   -- VariableStore.Put(DB,V1,10);
-
-   -- Put_Line("Reading the entry:");
-   -- Put(VariableStore.Get(DB,V1));
-   -- New_Line;
-   
-   -- Put_Line("Printing out the database: ");
-   -- VariableStore.Print(DB);
-   
-   -- Put_Line("Removing the entry");
-   -- VariableStore.Remove(DB,V1);
-   -- If VariableStore.Has_Variable(DB,V1) then
-   --    Put_Line("Entry still present! It is: ");
-   --    Put(VariableStore.Get(DB,V1));
-   --    New_Line;
-   -- else
-   --    Put_Line("Entry successfully removed");
-   -- end if;
-
-   -- Put_Line("Reading a line of input. Enter some text (at most 3 tokens): ");
-   -- Lines.Get_Line(S);
-
-   -- Put_Line("Splitting the text into at most 5 tokens");
-   -- declare
-   --    T : MyStringTokeniser.TokenArray(1..5) := (others => (Start => 1, Length => 0));
-   --    NumTokens : Natural;
-   -- begin
-   --    MyStringTokeniser.Tokenise(Lines.To_String(S),T,NumTokens);
-   --    Put("You entered "); Put(NumTokens); Put_Line(" tokens.");
-   --    for I in 1..NumTokens loop
-   --       declare
-   --          TokStr : String := Lines.To_String(Lines.Substring(S,T(I).Start,T(I).Start+T(I).Length-1));
-   --       begin
-   --          Put("Token "); Put(I); Put(" is: """);
-   --          Put(TokStr); Put_Line("""");
-   --       end;
-   --    end loop;
-   --    if NumTokens > 3 then
-   --       Put_Line("You entered too many tokens --- I said at most 3");
-   --    end if;
-   -- end;
-
-   -- If PIN."="(PIN1,PIN2) then
-   --    Put_Line("The two PINs are equal, as expected.");
-   -- end if;
-   
-   -- declare
-   --    Smallest_Integer : Integer := StringToInteger.From_String("-2147483648");
-   --    R : Long_Long_Integer := 
-   --      Long_Long_Integer(Smallest_Integer) * Long_Long_Integer(Smallest_Integer);
-   -- begin
-   --    Put_Line("This is -(2 ** 32) (where ** is exponentiation) :");
-   --    Put(Smallest_Integer); New_Line;
-      
-   --    if R < Long_Long_Integer(Integer'First) or
-   --       R > Long_Long_Integer(Integer'Last) then
-   --       Put_Line("Overflow would occur when trying to compute the square of this number");
-   --    end if;
-         
-   -- end;
-   -- Put_Line("2 ** 32 is too big to fit into an Integer...");
-   -- Put_Line("Hence when trying to parse it from a string, it is treated as 0:");
-   -- Put(StringToInteger.From_String("2147483648")); New_Line;
-   
       
 end Main;
