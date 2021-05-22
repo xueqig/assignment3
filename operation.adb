f1e5688a7005ccd9e317dae6bed7851d3eee0ed2
Xueqi Guan <xueqig@student.unimelb.edu.au>
2021-05-22 13:10:50 +1000

    fix integer overflow warnings

--- operation.adb
+++ operation.adb
@@ -1,3 +1,5 @@
+with Stack;
+
 package body OPERATION with SPARK_Mode is
    I : Integer := 0;
    J : Integer := 0;
@@ -5,34 +7,56 @@ package body OPERATION with SPARK_Mode is
     
    procedure Addition(S: in out Stack.Stack_Type) is
    begin
-      Stack.pop(S,I);
-      Stack.pop(S,J);
-      Result := I + J;
-      Stack.push(S,Result);  
+      Stack.Pop(S,I);
+      Stack.Pop(S,J);
+      
+      -- Check if addition will cause integer overflow
+      if (J < 1 and then I >= Integer'First - J) or (J > -1 and then I <= Integer'Last - J) then
+         Result := I + J;
+         Stack.Push(S, Result);
+      end if;
+       
    end Addition;
 
    procedure Subtraction(S: in out Stack.Stack_Type) is
    begin
-      Stack.pop(S,I);
-      Stack.pop(S,J);
-      Result := J - I;
-      Stack.push(S,Result);  
+      Stack.Pop(S,I);
+      Stack.Pop(S,J);
+      
+      -- Check if substraction will cause integer overflow
+      if (J > -1 and then I >= Integer'First + J) or (J < 1 and then I <= Integer'Last + J) then
+         Result := I - J;
+         Stack.Push(S, Result);  
+      end if;
    end Subtraction;
 
    procedure Multiplication(S: in out Stack.Stack_Type) is
    begin
-      Stack.pop(S,I);
-      Stack.pop(S,J);
-      Result := I * J;
-      Stack.push(S,Result);  
+      Stack.Pop(S,I);
+      Stack.Pop(S,J);
+      
+      -- Check if multiplication will cause integer overflow
+      if (J < -1 and then (I <= Integer'First / J and I >= Integer'Last / J)) or 
+        (J = -1 and I /= Integer'First) or 
+        (J = 0) or 
+        (J > 0 and then (I >= Integer'First / J and I <= Integer'Last / J)) then
+         Result := I * J;
+         Stack.push(S,Result);
+      end if;
+        
    end Multiplication;
 
    procedure Division(S: in out Stack.Stack_Type) is
    begin
-      Stack.pop(S,I);
-      Stack.pop(S,J);
-      Result := I / J;
-      Stack.push(S,Result);  
+      Stack.Pop(S,I);
+      Stack.Pop(S,J);
+      
+      if J /= 0 then 
+         if (J /= -1) or (J = -1 and I /= Integer'First) then
+            Result := I / J;
+            Stack.push(S,Result);  
+         end if;
+      end if;
    end Division;
 
 end OPERATION;
--- operation.ads
+++ operation.ads
@@ -2,20 +2,20 @@ with Stack;
 
 package OPERATION with SPARK_Mode is
 
-    procedure Addition(S: in out Stack.Stack_Type) with
-        Pre => (Stack.Get_Size(S) >= 2),
-        Post => (Stack.Get_Size(S) = (Stack.Get_Size(S'Old) - 1));
+   procedure Addition(S: in out Stack.Stack_Type) with
+     Pre => Stack.Get_Size(S) >= 2,
+     Post => (Stack.Get_Size(S) = (Stack.Get_Size(S'Old) - 1));
+   
+   procedure Subtraction(S: in out Stack.Stack_Type) with
+     Pre => (Stack.Get_Size(S) >= 2),
+     Post => (Stack.Get_Size(S) = (Stack.Get_Size(S'Old) - 1));
     
-    procedure Subtraction(S: in out Stack.Stack_Type) with
-        Pre => (Stack.Get_Size(S) >= 2),
-        Post => (Stack.Get_Size(S) = (Stack.Get_Size(S'Old) - 1));
-    
-    procedure Multiplication(S: in out Stack.Stack_Type) with
-        Pre => (Stack.Get_Size(S) >= 2),
-        Post => (Stack.Get_Size(S) = (Stack.Get_Size(S'Old) - 1));
-    
-    procedure Division(S: in out Stack.Stack_Type) with
-        Pre => (Stack.Get_Size(S) >= 2) and Stack.Get_Element(S, Stack.Get_Size(S) - 1) /= 0,
-        Post => (Stack.Get_Size(S) = (Stack.Get_Size(S'Old) - 1));
+   procedure Multiplication(S: in out Stack.Stack_Type) with
+     Pre => (Stack.Get_Size(S) >= 2),
+     Post => (Stack.Get_Size(S) = (Stack.Get_Size(S'Old) - 1));
+   
+   procedure Division(S: in out Stack.Stack_Type) with
+     Pre => (Stack.Get_Size(S) >= 2),
+     Post => (Stack.Get_Size(S) = (Stack.Get_Size(S'Old) - 1));
 
 end OPERATION;
